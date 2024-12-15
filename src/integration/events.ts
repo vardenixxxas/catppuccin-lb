ba712996b45aa7bfbab74d840ff9de0f2476160a
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2024-12-15 21:03:00 +0000

    chore: src template

---
+++ src/integration/events.ts
@@ -0,0 +1,106 @@
+import type {Component, ConfigurableSetting, Pointer, PlayerData, Proxy, Screen, Server, TextComponent} from "./types";
+
+export interface ClickGuiValueChangeEvent {
+    configurable: ConfigurableSetting;
+}
+
+export interface ModuleToggleEvent {
+    moduleName: string;
+    hidden: boolean;
+    enabled: boolean;
+}
+
+export interface KeyboardKeyEvent {
+    keyCode: number;
+    scanCode: number;
+    action: number;
+    mods: number;
+    key: string;
+    screen: Screen | undefined;
+}
+
+export interface ScaleFactorChangeEvent {
+    scaleFactor: number;
+}
+
+export interface ComponentsUpdateEvent {
+    components: Component[];
+}
+
+export interface ClientPlayerDataEvent {
+    playerData: PlayerData;
+}
+
+export interface OverlayMessageEvent {
+    text: TextComponent | string;
+    tinted: boolean;
+}
+
+export interface NotificationEvent {
+    title: string;
+    message: string;
+    severity: "INFO" | "SUCCESS" | "ERROR" | "ENABLED" | "DISABLED";
+}
+
+export interface KeyEvent {
+    key: string;
+    action: number;
+    mods: number;
+}
+
+export interface TargetChangeEvent {
+    target: PlayerData | null;
+}
+
+export interface BlockCountChangeEvent {
+    count?: number;
+}
+
+export interface PointerInfoEvent {
+    pointers?: Pointer[];
+}
+
+export interface AccountManagerAdditionEvent {
+    username: string | null;
+    error: string | null;
+}
+
+export interface AccountManagerMessageEvent {
+    message: string;
+}
+
+export interface AccountManagerLoginEvent {
+    username: string | null;
+    error: string | null;
+}
+
+export interface ServerPingedEvent {
+    server: Server;
+}
+
+export interface ProxyAdditionResultEvent {
+    proxy: Proxy | null;
+    error: string | null;
+}
+
+export interface ProxyEditResultEvent {
+    proxy: Proxy | null;
+    error: string | null;
+}
+
+export interface ProxyCheckResultEvent {
+    proxy: Proxy;
+    error: string | null;
+}
+
+export interface SpaceSeperatedNamesChangeEvent {
+    value: boolean;
+}
+
+export interface ClickGuiScaleChangeEvent {
+    value: number;
+}
+
+export interface BrowserUrlChangeEvent {
+    url: string;
+}
